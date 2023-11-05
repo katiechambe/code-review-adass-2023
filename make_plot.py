0f4dc1d2b44641e66159e0dc02c457b8437dc22a
katiechambe <katiechambe@email.arizona.edu>
2023-11-05 15:48:37 -0700

    added header info

--- make_plot.py
+++ make_plot.py
@@ -4,6 +4,12 @@ import os
 from astropy.io import fits
 import glob
 
+"""
+created: 5 Nov 2023
+-------------------
+Creates plot of fits data
+"""
+
 data = fits.getdata('/Users/bostroem/Desktop/images/hubble_img.fits')
 col1 = data[20, :]
 col2 = data[200, :]
